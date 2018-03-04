a822b39eca329d69f45a4cc0bf54d3a1d1d639f1
ahrrhy <ahrrhy@gmail.com>
2018-03-04 22:18:04 +0200

    started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AuthorizationPageComponent } from './authorization-page/authorization-page.component';
+import { SignInFormComponent } from './authorization-page/sign-in-form/sign-in-form.component';
+import { SignUpFormComponent } from './homepage/sign-up-form/sign-up-form.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import {RouterModule} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
+
+const routes = [
+  {path: '', component: HomepageComponent},
+  {path: 'authorization', component: AuthorizationPageComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorizationPageComponent,
+    SignInFormComponent,
+    SignUpFormComponent,
+    HomepageComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
